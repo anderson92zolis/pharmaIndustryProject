1dc58d85533056322d8650194633f79ee209634f
anderson92zolis <azolis92@hotmail.es>
2023-11-24 17:18:31 +0100

    add configSecurity

---
+++ security_module/src/main/java/com/apipharma/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.apipharma.security.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+}
--- security_module/src/main/java/com/apipharma/security/controller/GreatingController.java
+++ security_module/src/main/java/com/apipharma/security/controller/GreatingController.java
@@ -2,3 +2,5 @@ package com.apipharma.security.controller;
 
 public class GreatingController {
 }
+
+
