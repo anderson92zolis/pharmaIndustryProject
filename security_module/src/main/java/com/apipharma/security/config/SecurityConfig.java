be8549cc44192a5c9ebba12b3c6594898007a366
anderson92zolis <azolis92@hotmail.es>
2023-11-25 11:22:21 +0100

    adding security by ALI

--- security_module/src/main/java/com/apipharma/security/config/SecurityConfig.java
+++ security_module/src/main/java/com/apipharma/security/config/SecurityConfig.java
@@ -1,27 +1,80 @@
 package com.apipharma.security.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import com.apipharma.security.dao.UserDao;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAthFilter jwtAthFilter;
+
+    private final UserDao userDao;
+
     @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+            .csrf().disable()
+            .authorizeHttpRequests() // if  some endpoint need Authorize
+            .requestMatchers("/api/v1/auth/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authenticationProvider(authenticationProvider()) // using our AuthentificationProvider
+            .addFilterBefore(jwtAthFilter,  UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                return userDao.findUserByEmail(email);
+            }
+        };
+
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {  //  Data Access Object
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+        /* I will call it authentication manager and within  this mean I want to inject an object  of type authentication configuration, and I will call it config this authentication  configuration hold already the information about 1:40:04 the authentication manager so I will just return  config.getAuthenticationManager() all right
+         */
     }
 }
---
+++ security_module/src/main/java/com/apipharma/security/controller/AuthenticationController.java
@@ -0,0 +1,50 @@
+package com.apipharma.security.controller;
+
+import com.apipharma.security.config.JwtUtils;
+import com.apipharma.security.dao.UserDao;
+import com.apipharma.security.dto.AuthenticationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final UserDao userDao;
+
+    private final JwtUtils jwtUtils;  // this class generates the token
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        final UserDetails user= userDao.findUserByEmail(request.getEmail());
+        if( user != null) {
+            String token= jwtUtils.generateToken(user);
+            System.out.println(token);
+             return ResponseEntity.ok(token);
+        } else {
+            return ResponseEntity.status(400).body("Some error has ocurred");
+        }
+    }
+
+
+
+
+
+
+
+
+}
