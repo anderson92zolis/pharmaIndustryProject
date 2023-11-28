b0ec7d919d143ced6da02396726aa78cdd8b2907
anderson92zolis <azolis92@hotmail.es>
2023-11-28 21:39:54 +0100

    adding the docker-compose it works

--- security_module/src/main/java/com/apipharma/security/repository/UserRepository.java
+++ security_module/src/main/java/com/apipharma/security/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.apipharma.security.repository;
 
+
 import com.apipharma.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
